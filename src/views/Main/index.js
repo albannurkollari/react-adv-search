d3cd7cd3d69ddc8841a91318436d85516124b256
Dren Pallusha <drenpallusha@gmail.com>
2020-12-25 19:03:02 +0100

    added fifth description point

--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -29,29 +29,43 @@ import "./styles.css";
 
   ReactDOM.render(
     <>
-      <header><h2>React Advanced Search</h2></header>
+      <header>
+        <h2>React Advanced Search</h2>
+      </header>
       <main>
         <h3>So...how does it work?</h3>
-        <nav style={{marginBottom: '10px'}}>
-          <div className='step'>
-            1. Click the <Icons.Search style={{margin: '0 4px' }} width={16} height={16}/> to start searching.
+        <nav style={{ marginBottom: "10px" }}>
+          <div className="step">
+            1. Click the{" "}
+            <Icons.Search style={{ margin: "0 4px" }} width={16} height={16} />{" "}
+            to start searching.
           </div>
-          <div className='step'>
-            2. Once the search bar appears start searching for any desired country.
-            <i style={{fontSize: '10px'}}>(disclaimer: the country data is provided by a public API)</i>
+          <div className="step">
+            2. Once the search bar appears start searching for any desired
+            country.
+            <i style={{ fontSize: "10px" }}>
+              (disclaimer: the country data is provided by a public API)
+            </i>
           </div>
-          <div className='step'>
-            3. If your search yielded results, you can than start navigating with
+          <div className="step">
+            3. If your search yielded results, you can then start navigating
+            with
             <b> up</b>, <b>down</b>, <b>Home</b> and <b>End</b> keys.
           </div>
-          <div className='step'>
+          <div className="step">
             4. You can aswell <b>pin/unpin</b> the options list by clicking on
-            <Icons.Pin style={{margin: '0 4px'}} width={16} height={16} />.
+            <Icons.Pin style={{ margin: "0 4px" }} width={16} height={16} />.
+          </div>
+          <div className="step">
+            5. If you press <b>Esc</b> once, it will close the results panel.
+            Pressing <b>Esc</b> for the 2nd time clears the search completely!
           </div>
         </nav>
         <Search
           detached={Boolean(localStorage.searchDetached)}
-          rowsVisible={localStorage.searchRows && Number(localStorage.searchRows)}
+          rowsVisible={
+            localStorage.searchRows && Number(localStorage.searchRows)
+          }
           possibilities={Array.isArray(countries) ? countries : PEOPLE}
         />
       </main>
@@ -60,9 +74,13 @@ import "./styles.css";
           {/* FLATICON.COM BADGE */}
           <div>
             {"Icons made by "}
-            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>
+            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+              Freepik
+            </a>
             {" from "}
-            <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              www.flaticon.com
+            </a>
           </div>
         </Trademark>
       </footer>
